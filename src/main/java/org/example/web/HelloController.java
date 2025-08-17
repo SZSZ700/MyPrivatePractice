79283997e22d55723f0ac4bef3ca19f28ded9ade
Sharbel Zarzour <sharbel343@gmail.com>
2025-08-17 23:39:25 +0300

    data structure super advanced practice

--- src/main/java/org/example/web/HelloController.java
+++ src/main/java/org/example/web/HelloController.java
@@ -1,4 +1,13 @@
 package org.example.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring!";
+    }
 }
