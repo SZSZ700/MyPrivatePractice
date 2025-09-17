b9e44139ae7ef91d5f203c0220ab96e52aff43fb
Sharbel Zarzour <sharbel343@gmail.com>
2025-09-17 12:28:18 +0300

    data structure super advanced practice

--- src/main/java/org/example/Meeting.java
+++ src/main/java/org/example/Meeting.java
@@ -1,4 +1,44 @@
 package org.example;
 
+// Class representing a Meeting that holds virtual rooms
 public class Meeting {
+    // Head of a linked list of VRoom nodes
+    private Node<VRoom> rooms;
+
+    // Constructor: initializes the meeting with no rooms
+    public Meeting() {
+        this.rooms = null;
+    }
+
+    // Getter: returns the list of rooms
+    public Node<VRoom> getRooms() {
+        return rooms;
+    }
+
+    // Setter: sets the list of rooms
+    public void setRooms(Node<VRoom> rooms) {
+        this.rooms = rooms;
+    }
+
+    // Override of toString: builds a string representation of all rooms
+    @Override
+    public String toString() {
+        // StringBuilder used for efficient string concatenation
+        var str = new StringBuilder();
+        // Temporary pointer to traverse the linked list of rooms
+        var pos = this.rooms;
+
+        // Iterate through all rooms
+        while (pos != null) {
+            // Append the current room's value
+            str.append(pos.getValue());
+            // Add a space after each room
+            str.append(" ");
+            // Move to the next room in the list
+            pos = pos.getNext();
+        }
+
+        // Return the final string
+        return str.toString();
+    }
 }
