c052183f5484256655ebcbb0f0e943e79613375d
Sharbel Zarzour <sharbel343@gmail.com>
2025-11-21 18:22:36 +0200

    data structure super advanced practice

---
+++ src/main/java/org/example/QrGenerator/QrGenerator.java
@@ -0,0 +1,38 @@
+package org.example.QrGenerator;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+public class QrGenerator {
+    public static void main(String[] args) throws WriterException, IOException {
+        // QrCode Writer object
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+
+        // create the matrix that represent the QR
+        BitMatrix bitMatrix = qrCodeWriter
+                .encode(
+                        "https://github.com/SZSZ700/MyPrivatePractice.git",
+                        BarcodeFormat.QR_CODE,
+                        500,
+                        500
+                );
+
+        // A Path represents a path that is hierarchical and composed
+        // of a sequence of directory and file name elements separated by "/"
+        Path path = FileSystems.getDefault()
+                .getPath(
+                        "System.getProperty(\"user.home\") + \"/Desktop/MY_QR.png\""
+                );
+
+        // object that create picture out of BitMatrix (that represent the QR code)
+        // write the Qr into PNG file
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+
+    }
+}
