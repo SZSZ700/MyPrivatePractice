8aafe12f4c53255c097a6eb2549eac8a1650edca
Sharbel Zarzour <sharbel343@gmail.com>
2026-01-02 21:51:04 +0200

    Advanced Data Structures practice

--- src/main/java/org/example/Q4/Client.java
+++ src/main/java/org/example/Q4/Client.java
@@ -4,6 +4,8 @@ public class Client {
     private String name;
     private int diners; // num of diners
 
+    public Client() { }
+
     public Client(String name, int diners) {
         this.name = name;
         this.diners = diners;
