85325c83f59eb22a07371b1a730eb05bbde1365d
Sharbel Zarzour <sharbel343@gmail.com>
2026-01-07 16:27:25 +0200

    Advanced Data Structures practice

--- src/main/java/org/example/Q4/secMain.java
+++ src/main/java/org/example/Q4/secMain.java
@@ -11,11 +11,10 @@ class tryMain {
     public static void main(String[] args) {
 
         // Create a Spring ApplicationContext using the Java configuration class (AppConfig).
-        AnnotationConfigApplicationContext ctx =
-                new AnnotationConfigApplicationContext(AppConfig.class);
+        var ctx = new AnnotationConfigApplicationContext(AppConfig.class);
 
         // Ask Spring for the managed Restaurant bean (singleton by default).
-        Restaurant r = ctx.getBean(Restaurant.class);
+        var r = ctx.getBean(Restaurant.class);
 
         // Create and enqueue a client group of 4 diners using a prototype Client bean.
         r.addClient("Avi", 4);
