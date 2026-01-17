317bfb5d5dc18e1a09d28a195e7ff6bca2c18516
Sharbel Zarzour <sharbel343@gmail.com>
2026-01-17 16:26:24 +0200

    C++ advanced Data Structures

--- StarCount/StarCount.cpp
+++ StarCount/StarCount.cpp
@@ -226,3 +226,119 @@ std::string StarCount::toString() const { // Converts the number list into a str
     return ss.str(); // Return the final string containing all digits
 }
 
+// ReSharper disable once CppMemberFunctionMayBeConst
+Node<int *> *StarCount::buildlist(const int *num)  {
+    assert(num != nullptr); // If the input pointer is null, do nothing and leave the list empty
+    // head pointer for the new list
+    Node<int*> *head = nullptr;
+    // tail pointer for the new list
+    Node<int*> *taily = nullptr;
+
+    const auto temp = new int(*num); // Create a copy of the original number on the heap
+
+    const auto stk = new std::stack<int*>(); // Create an auxiliary stack to reverse the digit order
+
+    while (*temp > 0) { // Loop while there are still digits in the number
+        auto currentDigit = new int(*temp % 10); // Extract the last digit and allocate it on the heap
+        stk->push(currentDigit); // Push the digit pointer onto the stack
+        *temp /= 10; // Remove the last digit from the copied number
+    }
+
+    delete temp; // Delete the copied number after all digits were extracted
+
+    while (!stk->empty()) { // While the stack still holds digits
+        const auto currentDigit = stk->top(); // Get the top digit pointer from the stack
+        auto toAdd = new Node(currentDigit); // Create a new list node that stores this digit pointer
+
+        if (head == nullptr) { // If the list is currently empty
+            head = toAdd; // Set head pointer to the newly created node
+            taily = toAdd; // Set tail pointer to the same node
+        } else { // If the list is not empty
+            assert(taily != nullptr); // Ensure the tail pointer is not null
+            taily->setNext(toAdd); // Link the new node after the current tail
+            taily = toAdd; // Move the tail pointer to the new node
+        }
+
+        stk->pop(); // Remove the top digit pointer from the stack
+    }
+
+    delete stk; // Delete the auxiliary stack
+
+    return head;
+}
+
+// ReSharper disable once CppMemberFunctionMayBeConst
+void StarCount::fixNumber() {
+    [[maybe_unused]] auto sizeOfNumber = [&](const int *num) -> int { // Lambda to calculate how many digits the number has
+        auto size = 0; // Counter for the number of digits
+        auto temp = *num; // Local copy of the number value
+
+        while (temp > 0) { // Loop while there are still digits
+            temp /= 10; // Remove the last digit
+            size++; // Increase the digit counter
+        }
+
+        return size; // Return the total number of digits
+    };
+
+    // If the head node holds a multi-digit number
+    if (this->number && this->number->getValue() && sizeOfNumber(this->number->getValue()) > 1) {
+        const Node<int*>* oldHead = this->number; // Store the old head pointer
+        // Store the node that comes after the old head in the original list
+        const auto after = oldHead->getNext();
+        // Build a new list of single-digit nodes from the head value
+        const auto Head = buildlist(oldHead->getValue());
+        // Start from the head of the new list
+        auto Tail = Head;
+
+        // Move Tail to the last node of the new list
+        while (Tail->getNext() != nullptr) { Tail = Tail->getNext(); }
+        // Connect the last node of the new list to the original 'after' node
+        Tail->setNext(after);
+
+        // Delete the integer stored in the old head node
+        delete oldHead->getValue();
+        // Delete the old head node itself
+        delete oldHead;
+        // Update the list head to point to the new head
+        this->number = Head;
+    }
+
+    // at the middle or at the end
+    auto pos = this->number; // Start from the (possibly updated) head
+    while (pos != nullptr && pos->getNext() != nullptr) { // Traverse as long as there is a next node
+
+        if (pos->getNext() && pos->getNext()->getValue() &&
+            sizeOfNumber(pos->getNext()->getValue()) == 1) { // If the next node holds a single-digit number
+            pos = pos->getNext(); // Move forward without changing anything
+        } else { // The next node holds a multi-digit number and needs to be split
+
+            const auto before = pos; // Node before the multi-digit node
+            const auto middle = pos->getNext(); // The multi-digit node itself
+            const auto after = pos->getNext()->getNext(); // Node after the multi-digit node (may be nullptr)
+
+            const auto new_list = buildlist(middle->getValue()); // Build a list of single-digit nodes from the middle value
+            auto tail_for_new_list = new_list; // Start from the head of the new sublist
+
+            while (tail_for_new_list->getNext() != nullptr) { // Move to the last node of the new sublist
+                tail_for_new_list = tail_for_new_list->getNext(); // Advance the tail pointer
+            }
+
+            before->setNext(new_list); // Connect the 'before' node to the head of the new sublist
+            tail_for_new_list->setNext(after); // Connect the end of the new sublist to the 'after' node
+
+            delete middle->getValue(); // Delete the integer stored in the old multi-digit node
+            delete middle; // Delete the old multi-digit node itself
+
+            pos = tail_for_new_list; // Continue traversal from the last node of the newly inserted sublist
+        }
+    }
+
+    // Recalculate tail pointer to keep it valid after all modifications
+    this->tail = nullptr; // Reset the tail pointer
+    auto scan = this->number; // Start scanning from the head
+    while (scan != nullptr) { // Traverse the entire list
+        this->tail = scan; // Update tail to the current node
+        scan = scan->getNext(); // Move to the next node
+    }
+}
