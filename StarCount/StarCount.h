6eb34cec12aac23f7fbac1e1242adc076b927a75
Sharbel Zarzour <sharbel343@gmail.com>
2026-01-17 15:01:11 +0200

    C++ advanced Data Structures

--- StarCount/StarCount.h
+++ StarCount/StarCount.h
@@ -1,8 +1,37 @@
-//
-// Created by Sharb on 1/17/2026.
-//
-
 #ifndef UNTITLED1_STARCOUNT_H
 #define UNTITLED1_STARCOUNT_H
+#include "..//Node/Node.h"
+class StarCount {
+    // pointer for the head of the list that represent the number
+    Node<int*> *number{};
+    // pointer for the tail of the list that represent the number
+    Node<int*> *tail{};
+public:
+    // constructor
+    explicit StarCount(const int *num);
 
+    // destructor
+    ~StarCount();
+
+    // copy constructor
+    StarCount(const StarCount &other);
+
+    // copy assignment
+    StarCount& operator=(const StarCount &other) noexcept;
+
+    // move constructor
+    StarCount(StarCount &&other) noexcept;
+
+    // move assignment
+    StarCount& operator=(StarCount &&other) noexcept;
+
+    // get
+    const Node<int*> *getNumber() const;
+
+    // set
+    void setNumber(const Node<int*> *numberr);
+
+    // tostring
+    std::string toString() const;
+};
 #endif //UNTITLED1_STARCOUNT_H
