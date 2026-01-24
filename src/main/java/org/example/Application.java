16f8697cd400d7b62bb9fc190579430078148d21
Sharbel Zarzour <sharbel343@gmail.com>
2026-01-24 17:00:35 +0200

    Advanced Data Structures practice

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,6 +1,5 @@
 // Define the package of the Spring Boot application
 package org.example;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
