c503a75b0f38850e2aa648d1a9e6bd23225853c6
Sharbel Zarzour <sharbel343@gmail.com>
2026-04-01 23:56:26 +0300

    C++ advanced Data Structures

--- RestaurantQ/Restaurant.h
+++ RestaurantQ/Restaurant.h
@@ -1,44 +1,43 @@
 #ifndef UNTITLED1_RESTAURANT_H
 #define UNTITLED1_RESTAURANT_H
-
 #include <list>
 #include <memory>
 #include <queue>
 #include <string>
 #include "Client.h"
 #include "Table.h"
+using namespace std;
 
 class Restaurant {
     // Queue that owns all waiting clients
-    std::queue<std::unique_ptr<Client>> clients;
-
+    queue<unique_ptr<Client>> clients;
     // List that owns all restaurant tables
-    std::list<std::unique_ptr<Table>> tables;
+    list<unique_ptr<Table>> tables;
 
 public:
     // Constructor that creates all restaurant tables
     Restaurant(int smallTables, int mediumTables, int largeTables);
 
     // Getter that returns a const reference to the client queue
-    const std::queue<std::unique_ptr<Client>>& getClients() const;
+    const queue<unique_ptr<Client>>& getClients() const;
 
     // Getter that returns a const reference to the table list
-    const std::list<std::unique_ptr<Table>>& getTables() const;
+    const list<unique_ptr<Table>>& getTables() const;
 
     // Setter that replaces the client queue by taking ownership
-    void setClients(std::queue<std::unique_ptr<Client>> clients);
+    void setClients(queue<std::unique_ptr<Client>> clients);
 
     // Setter that replaces the table list by taking ownership
-    void setTables(std::list<std::unique_ptr<Table>> tables);
+    void setTables(list<std::unique_ptr<Table>> tables);
 
     // Adds a new client to the waiting queue
-    void addClient(std::unique_ptr<Client> client);
+    void addClient(unique_ptr<Client> client);
 
     // Adds a new table to the restaurant
-    void addTable(std::unique_ptr<Table> table);
+    void addTable(unique_ptr<Table> table);
 
     // Returns a readable string that describes the restaurant
-    std::string toString() const;
+    string toString() const;
 
     // Finds a table number with enough free places for the given number of diners
     int findAvailableTable(int numOfDiners) const;
