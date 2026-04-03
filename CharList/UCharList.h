caa3ca285c4091fa13ee58ceb1213b7ef923c619
Sharbel Zarzour <sharbel343@gmail.com>
2026-04-03 21:51:11 +0300

    C++ advanced Data Structures

--- CharList/UCharList.h
+++ CharList/UCharList.h
@@ -1,8 +1,17 @@
-//
-// Created by Sharb on 4/3/2026.
-//
-
 #ifndef UNTITLED1_UCHARLIST_H
 #define UNTITLED1_UCHARLIST_H
+#include <memory>
+#include <list>
+using namespace std;
 
+class UCharList {
+    list<unique_ptr<char>> chain;
+
+    public:
+    // constructor
+    explicit UCharList(list<unique_ptr<char>> &otherList);
+
+    // make the list valid
+    void swap(char letter);
+};
 #endif //UNTITLED1_UCHARLIST_H
